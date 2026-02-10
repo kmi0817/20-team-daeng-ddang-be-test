705567396b2c90f2ca63fab7c2ab5df29ddb78ca
kaa08 <kaa08@naver.com>
2026-02-10 09:23:53 +0900

    feat: 반려견 헬스케어 분석 domain / repository 구현

--- src/main/java/com/daengddang/daengdong_map/repository/AnalysisRepository.java
+++ src/main/java/com/daengddang/daengdong_map/repository/AnalysisRepository.java
@@ -1,13 +1,8 @@
 package com.daengddang.daengdong_map.repository;
 
 import com.daengddang.daengdong_map.domain.analysis.Analysis;
-import com.daengddang.daengdong_map.repository.projection.DailyRecordView;
-import com.daengddang.daengdong_map.repository.projection.DateCountView;
-import java.time.LocalDateTime;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 
 public interface AnalysisRepository extends JpaRepository<Analysis, Long> {
 
